c2e9178e3875f13839664a093ede9450511f42ca
Jesus Ramirez <jrmz.maldonado@gmail.com>
2020-07-20 13:03:11 -0500

    delay on footer showing

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ function loading() {
 function showPage() {
     document.querySelector('.loader').style.display = 'none';
     document.querySelector('.main-container').style.display = 'block';
-    document.querySelector('.footer').style.display = 'block';
+    setTimeout(function(){
+        document.querySelector('.footer').style.display = 'block';
+    },3000);
 }
 const toProjects = document.querySelector('#toProjects');
 const projects = document.querySelector('#projects');
