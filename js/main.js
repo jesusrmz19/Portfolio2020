f5d18199088e97f20d97f919cd977d5a48d1acf9
Jesus Ramirez <jrmz.maldonado@gmail.com>
2020-07-20 13:47:39 -0500

    incremented delay

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 var waiting;
 function loading() {
-    waiting = setTimeout(showPage, 3000);
+    waiting = setTimeout(showPage, 4000);
 }
 function showPage() {
     document.querySelector('.loader').style.display = 'none';
     document.querySelector('.main-container').style.display = 'block';
     setTimeout(function(){
         document.querySelector('.footer').style.display = 'block';
-    },3000);
+    },4000);
 }
 const toProjects = document.querySelector('#toProjects');
 const projects = document.querySelector('#projects');
