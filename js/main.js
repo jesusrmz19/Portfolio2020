b7e06d8e726d4b4c50ee7d8cf8d8393b96249943
Jesus Ramirez <jrmz.maldonado@gmail.com>
2020-07-20 12:55:06 -0500

    loader added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+var waiting;
+function loading() {
+    waiting = setTimeout(showPage, 3000);
+}
+function showPage() {
+    document.querySelector('.loader').style.display = 'none';
+    document.querySelector('.main-container').style.display = 'block';
+    document.querySelector('.footer').style.display = 'block';
+}
 const toProjects = document.querySelector('#toProjects');
 const projects = document.querySelector('#projects');
 function projectsIntoView(){
