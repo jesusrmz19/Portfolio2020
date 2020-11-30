daff7c13135f8816b5f684d80aec83cd7e3489dc
Jesus Ramirez <jrmz.maldonado@gmail.com>
2020-11-30 17:50:11 -0600

    scrolltrigger animation added to all the webpage

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,78 @@
 var waiting;
 var maximumScroll;
 
+/* GSAP FUNCTIONALITY */
+gsap.registerPlugin(ScrollTrigger);
+const firstFoldTl = gsap.timeline(); 
+window.addEventListener('load', function () {
+    firstFoldTl.from(".first-fold-txt", {duration: 0.8, x: -200, opacity: 0})
+    .from(".first-fold-btn", {duration: 0.4, y: 100, opacity: 0}, "-=0.20"); 
+})
+
+const projects = gsap.utils.toArray('.project');
+projects.forEach((self) => {
+    let projectImg = self.querySelectorAll('.project-img');
+    let projectName = self.querySelectorAll('.project-name');
+    let projectDesc = self.querySelectorAll('.project-description');
+    let projectBtns = self.querySelectorAll('.project-btns');
+    let projectTl = gsap.timeline();
+
+    projectTl.from(projectImg, {duration: 0.5, x: -100, opacity: 0})
+    .from(projectName, {duration: 0.4, x: 100, opacity: 0}, "-=0.20")
+    .from(projectDesc, {duration: 0.4, x: 100, opacity: 0}, "-=0.20")
+    .from(projectBtns, {duration: 0.4, x: 100, opacity: 0}, "-=0.20");
+
+    ScrollTrigger.create({
+        animation: projectTl,
+        trigger: self,
+        start: "20% 50%",
+    });
+
+});
+
+const experiments = gsap.utils.toArray('.experiment');
+experiments.forEach((self) => {
+    gsap.from(self, {
+        scrollTrigger: {
+            trigger: self,
+            start: "top 75% "
+        },
+        opacity: 0,
+        y: 150,
+        duration: 0.4,
+        stagger: 0.1
+    });
+});
+
+gsap.from(".about", {
+    scrollTrigger: {
+        trigger: ".about",
+        start: "top: 60%"
+    },
+    opacity: 0,
+    x: -100,
+    duration: 0.6
+});
+gsap.from(".skills", {
+    scrollTrigger: {
+        trigger: ".skills",
+        start: "top: 60%"
+    },
+    opacity: 0,
+    x: 100,
+    duration: 0.6
+});
+
+const contactTl = gsap.timeline();
+contactTl.from(".contact-title", { duration: 0.4, opacity: 0, y: 100 })
+    .from(".contact-subtitle", { duration: 0.4, opacity: 0, y: 100 })
+    .from(".form-container", { duration: 0.4, opacity: 0, y: 100 });
+ScrollTrigger.create({
+    trigger: ".contact",
+    start: "top 60%"
+});
+
+
 /* Mobile Menu */
 function openMobileMenu() {
     document.querySelector('.overlay-container').classList.toggle('open');
@@ -45,7 +117,4 @@ function showUpBtn() {
 }
 window.addEventListener('scroll', showUpBtn);
 
-/* GSAP FUNCTIONALITY */
-gsap.from(".first-fold-txt", {duration: 1, x: -300, opacity: 0});
-gsap.from(".first-fold-btn", {duration: 1, x: -300, opacity: 0});
 
