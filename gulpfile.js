13f613252d1da6453b88fe63703da9549f9f2aa5
Jesus Ramirez <jrmz.maldonado@gmail.com>
2021-04-07 08:57:39 -0500

    Add images to gulp pipeline

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 const browserSync = require('browser-sync').create();
 
 function watch() {
-    browserSync.init({
-        server: {
-            baseDir: 'src'
-        }
-    });
-    gulp.watch('src/assets/css/**/*.css').on('change', browserSync.reload);
-    gulp.watch('src/*html').on('change', browserSync.reload);
-    gulp.watch('src/assets/js/**/*.js').on('change', browserSync.reload);
+  browserSync.init({
+    server: {
+      baseDir: 'src',
+    },
+  });
+  gulp.watch('src/assets/css/**/*.css').on('change', browserSync.reload);
+  gulp.watch('src/*html').on('change', browserSync.reload);
+  gulp.watch('src/assets/js/**/*.js').on('change', browserSync.reload);
 }
 
 exports.watch = watch;
-
 
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
@@ -23,24 +22,19 @@ const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const { src, parallel, dest } = require('gulp');
 
-
 const jsPath = 'src/assets/js/**/*.js';
 const cssPath = 'src/assets/css/**/*.css';
 
 function buildHTML() {
-    return src('src/*.html').pipe(gulp.dest('dist'));
+  return src('src/*.html').pipe(gulp.dest('dist'));
 }
 
 function exportDocs() {
-    return src('src/assets/docs/*').pipe(gulp.dest('dist/assets/docs'));
-}
-
-function imgTask() {
-    return src('src/images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'));
+  return src('src/assets/docs/*').pipe(gulp.dest('dist/assets/docs'));
 }
 
 function jsTask() {
-    return src(jsPath)
+  return src(jsPath)
     .pipe(sourcemaps.init())
     .pipe(concat('main.js'))
     .pipe(terser())
@@ -49,7 +43,7 @@ function jsTask() {
 }
 
 function cssTask() {
-    return src(cssPath)
+  return src(cssPath)
     .pipe(sourcemaps.init())
     .pipe(concat('main.css'))
     .pipe(postcss([cssnano()]))
@@ -58,19 +52,28 @@ function cssTask() {
 }
 
 function fontsTask() {
-    return src('src/assets/fonts/**/*').pipe(gulp.dest('dist/assets/fonts'));
+  return src('src/assets/fonts/**/*').pipe(gulp.dest('dist/assets/fonts'));
 }
 
-
+function imgTask() {
+  return src('src/images/*').pipe(imagemin()).pipe(gulp.dest('dist/images'));
+}
 
 /*function watchTask() {
     watch([cssPath, jsPath], { interval: 1000 }, parallel(cssTask, jsTask));
 }*/
 
-
 exports.exportDocs = exportDocs;
 exports.cssTask = cssTask;
 exports.jsTask = jsTask;
 exports.fontsTask = fontsTask;
+exports.imgTask = imgTask;
 
-exports.build = parallel(buildHTML, cssTask, jsTask, exportDocs, fontsTask);
+exports.build = parallel(
+  buildHTML,
+  cssTask,
+  jsTask,
+  exportDocs,
+  fontsTask,
+  imgTask
+);
